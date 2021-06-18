218c742af971fcb2408cf5879047b282d873f03c
adrianpruneda <42491636+adrianpruneda@users.noreply.github.com>
2021-06-18 14:26:31 -0600

    Update index.js

--- index.js
+++ index.js
@@ -56,7 +56,13 @@ const ScalableImage = props => {
             const sourceToUse = source.uri
                 ? source.uri
                 : source;
-           
+            
+            Image.getSize(
+                sourceToUse,
+                (width, height) => adjustSize(width, height, props),
+                console.err
+            );
+            
         }
         else {
             const sourceToUse = resolveAssetSource(source);
